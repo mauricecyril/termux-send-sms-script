39ec5963a5f371d4f5278626c287d4fffc88cc53
mauricecyril <25261554+mauricecyril@users.noreply.github.com>
2019-03-04 21:05:49 -0500

    Update sendsms.py

--- sendsms.py
+++ sendsms.py
@@ -1,1 +1,22 @@
+import subprocess
 
+# Add Entries to the address book dictionary. Key = Name, Value = Phone Number
+addressbook = {"Name1" : "+15551234567"
+                "Name2" : "+15551234568"
+                }
+                
+# Loop through the addressbook dictionary and send each number the message
+for (k,v) in addressbook.items():
+    
+    # SMS Message Template (try to keep to within 150 characters)
+    smsmessage = str("Hi " + k + " your phone number is " + v)
+    
+    # Use Subprocess Run Function to send SMS
+    subprocess.run(["termux-sms-send", "-n", phonenumber, smsmessage])
+    
+    # Print confirmation of each send
+    print("Sent Message to " + k + " via " + v)
+
+
+# Print end of process message
+print("Message sending complete")
